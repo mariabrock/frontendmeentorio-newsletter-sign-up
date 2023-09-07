ee235409344adcf3c997e83913334b62e25949cd
Maria Brock <brockm@ornl.giv>
2023-09-07 16:51:13 -0400

    mobile now works

--- main.js
+++ main.js
@@ -2,33 +2,39 @@ const submitBtn = document.querySelector('.submit-btn');
 const dismissBtn = document.querySelector('.dismiss-btn');
 const successCard = document.querySelector('.success');
 const previewCard = document.querySelector('.preview');
-// const fakeForm = document.querySelector('.fake-form');
-// const pattern = "[A-Za-z0-9._+-]+@[A-Za-z0-9 -]+\\.[a-z]{2,}"
-// const errorMessage = document.querySelector('#error-msg');
+const emailInput = document.querySelector('#email');
 
-// emailInput.addEventListener('keydown', () => {
-//   if(userEmail.match(pattern)) {
-//    emailInput.classList.toggle("valid");
-//    emailInput.classList.toggle("invalid");
-//    emailInput.style.border = "#00ff00";
-//   } else {
-//    emailInput.classList.toggle("valid");
-//    emailInput.classList.toggle("invalid");
-//    errorMessage.innerHTML = "Valid Email Required";
-//    errorMessage.style.color = "#ff0000";
-//   }
-//   if(emailInput.value === null) {
-//    emailInput.classList.remove("valid");
-//    emailInput.classList.remove("invalid");
-//    errorMessage.remove();
-//   }
-// })
+
+emailInput.addEventListener('keydown', () => {
+ const emailInput = document.querySelector('#email');
+ const userEmail = emailInput.value;
+ // const fakeForm = document.querySelector('.fake-form');
+ const errorMessage = document.querySelector('#error-msg');
+ const pattern = "[A-Za-z0-9._+-]+@[A-Za-z0-9 -]+\\.[a-z]{2,}"
+  if(userEmail.match(pattern)) {
+   emailInput.classList.add("valid");
+   emailInput.classList.remove("invalid");
+   emailInput.style.color = "#000000";
+   errorMessage.innerHTML = '';
+  } else {
+   emailInput.classList.add("invalid");
+   emailInput.classList.remove("valid");
+   errorMessage.innerHTML = "Valid Email Required";
+   errorMessage.style.color = "#ff0000";
+  }
+  if(emailInput.value === null) {
+   emailInput.classList.remove("valid");
+   emailInput.classList.remove("invalid");
+   errorMessage.innerHTML = '';
+  }
+})
 
 submitBtn.addEventListener('click', () => {
  const emailInput = document.querySelector('#email');
  const userEmail = emailInput.value;
  if(userEmail === '') {
 	 alert("Please enter an email address!")
+  emailInput.style.border = "solid red 2px";
  } else {
   console.log(userEmail);
   successCard.style.visibility = 'visible';
